0b68e3c4f63f2aca92f6611b2632cee8df88d326
AbhishekVernekar <abhi.abhishekvernekar@gmail.com>
2020-04-20 13:47:05 +0530

    ReactAssignment8 20April

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clicks: 0,
+      show: true
+    };
+  }
+
+  IncrementItem = () => {
+    this.setState({ clicks: this.state.clicks + 1 });
+  }
+  DecreaseItem = () => {
+    this.setState({ clicks: this.state.clicks - 1 });
+  }
+  ToggleClick = () => {
+    this.setState({ show: !this.state.show });
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.IncrementItem}>Increase by 1 unit</button> &nbsp;
+
+        <button onClick={this.DecreaseItem}>Decrease by 1 unit</button>
+
+        <button onClick={this.ToggleClick}>
+          { this.state.show ? 'Hide' : 'Show' }
+        </button>
+        { this.state.show ? <h2>{ this.state.clicks }</h2> : '' }
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import EmpComp from './EmpComp';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-  <EmpComp/>
+  <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
