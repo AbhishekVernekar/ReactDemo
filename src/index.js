ddb4cbb1bb8bb7cfa480fb833aa4e1e7d866acaf
AbhishekVernekar <abhi.abhishekvernekar@gmail.com>
2020-04-23 15:15:38 +0530

    Login App

--- src/index.js
+++ src/index.js
@@ -3,10 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import LoginComponent from './Login';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import RegisterComponent from './Register';
+import DashboardComponent from './Dashboard';
 
+const RouterComponent = () => (
+  <Router>
+    <Route exact path='/' component={LoginComponent}></Route>
+    <Route path='/login' component={LoginComponent}></Route>
+    <Route path='/register' component={RegisterComponent}></Route>
+    <Route path='/dashboard' component={DashboardComponent}></Route>
+  </Router>
+)
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <RouterComponent />
   </React.StrictMode>,
   document.getElementById('root')
 );
