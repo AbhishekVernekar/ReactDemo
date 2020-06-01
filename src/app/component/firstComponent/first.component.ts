e84a52885cc517892d5abedd5c1dc682240580ed
AbhishekVernekar <abhi.abhishekvernekar@gmail.com>
2020-06-01 19:58:55 +0530

    FirstAngularAssignment

---
+++ src/app/component/firstComponent/first.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'first-component',
+  templateUrl: './first.component.html',
+  styleUrls: []
+})
+export class FirstComponent {
+  title = 'FirstComponent';
+  count = 0;
+} 
+
